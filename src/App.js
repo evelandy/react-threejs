1107fa197ec17eb0e48548aa5922a366c6f368fb
William Griffin <40611326+evelandy@users.noreply.github.com>
2022-08-24 12:24:05 -0500

    edited to cone

--- src/App.js
+++ src/App.js
@@ -8,20 +8,29 @@ class App extends Component {
     var scene = new THREE.Scene();
     var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
     var renderer = new THREE.WebGLRenderer();
+
     renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.appendChild( renderer.domElement );
-    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-    var cube = new THREE.Mesh( geometry, material );
-    scene.add( cube );
+
+    // var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+    // var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    // var cube = new THREE.Mesh( geometry, material );
+    // scene.add( cube );
+
+    const geometry = new THREE.ConeGeometry( 2, 2, 10 );
+    const material = new THREE.MeshBasicMaterial( {color: 0xffff00} );
+    const cone = new THREE.Mesh( geometry, material );
+    scene.add( cone );
+
     camera.position.z = 5;
-    var animate = function () {
-      requestAnimationFrame( animate );
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
-      renderer.render( scene, camera );
-    };
-    animate();
+
+      var animate = function () {
+        requestAnimationFrame( animate );
+        cone.rotation.x += 0.01;
+        cone.rotation.y += 0.01;
+        renderer.render( scene, camera );
+      };
+      animate();
     // === THREE.JS EXAMPLE CODE END ===
   }
   render() {
